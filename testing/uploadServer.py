d1cfc58ac038901908561094c6df90a626fea3ee
Shrinidhi Rao <shrinidhi666@gmail.com>
2015-09-10 19:23:01 +0530

    fixing

---
+++ testing/uploadServer.py
@@ -0,0 +1,105 @@
+from twisted.web.server import Site
+from twisted.web.static import File
+from twisted.internet import reactor
+from twisted.web.resource import Resource
+import re
+import inspect
+import argparse
+import os
+import shutil
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p","--path",dest='rootPath',help='root dir of http server')
+args = parser.parse_args()
+
+clientsAllowed = {}
+
+class Registered(Resource):
+  isLeaf = True
+  def render_GET(self, request):
+    return "<html>REGISTERED!</html>"
+
+class NotRegistered(Resource):
+  isLeaf = True
+  def render_GET(self, request):
+    return "<html>NOT REGISTERED!</html>"
+
+class UploadDone(Resource):
+  isLeaf = True
+  allowedMethods = ('POST',)
+  def getChild(self,name,request):
+    print("in getChild of Upload")
+    return(Resource.render_POST(self,request))
+  # def render(self, request):
+    # return "<html>Upload Done!</html>"
+
+
+class myfile(File):
+  allowedMethods = ('GET','POST','HEAD')
+  def __init__(self,*args):
+
+    # try:
+    print("in myfile : "+ str(args))
+    # except:
+    #   pass
+    File.__init__(self,*args)
+
+
+  def getChild(self, name, request):
+    print("in getChild : "+ str(name) +":"+ str(request))
+    # print(dir(request))
+    print(request.method)
+    print(request.getClientIP())
+    if(request.getClientIP() in clientsAllowed.keys()):
+      if(str(request.method) == "POST"):
+        print("in post method")
+        print(request.getAllHeaders())
+        print(request.getAllHeaders()['file-name'])
+        # # print(dir(request))
+        # # print(dir(request.content.file))
+        # # request.content.file.seek(0,0)
+        # try:
+        #   os.makedirs(request.getAllHeaders()['dest-dir'])
+        # except:
+        #   pass
+        # fw = open(request.getAllHeaders()['dest-dir']+ os.sep + request.getAllHeaders()['file-name'],'wb')
+        # # request.content.seek(0,0)
+        # fw.write(request.content.read())
+        # fw.flush()
+        # fw.close()
+        return(UploadDone())
+      else:
+        test = File.getChild(self,name,request)
+        print(test)
+        return(test)
+    elif(re.search('^/REGISTER',request.uri)):
+      clientsAllowed[request.getClientIP()] = 1
+      return(Registered())
+    else:
+      return(NotRegistered())
+
+
+    
+
+  def render_GET(self,request):
+    # 
+    print("in get : "+ str(request))
+    print("----------------------")
+    print(request.getClientIP() +" : HEADERS")
+    print("----------------------")
+    print(request.getAllHeaders())
+    # for x in request.headers.keys():
+    #   print(x +" : "+ request.headers[x])
+    print("----------------------")
+    return File.render_GET(self,request)
+
+# resource = File('./')
+if(args.rootPath):
+  res = myfile(args.rootPath)   
+else:
+  res = myfile("./")
+# print(dir(res))
+factory = Site(res)
+reactor.listenTCP(80, factory)
+reactor.run()
---
+++ testing/uploadTest.py
@@ -0,0 +1,35 @@
+import requests
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--infile",dest='inputfile',help='input file name')
+parser.add_argument("-d","--destdir",dest='destdir',help='destination dir in the server with respect to its root dir')
+parser.add_argument("-o","--outname",dest='outname',help='name for the copied file in the destination')
+args = parser.parse_args()
+
+if(args.inputfile):
+  fileToUpload = os.path.abspath(args.inputfile)
+else:
+  fileToUpload = "D:\\resume_official.pdf"
+if(args.destdir):
+  destDir = args.destdir
+else:
+  destDir = "testingUpload"
+if(args.outname):
+  destFileName = args.outname
+else:
+  destFileName = "fuckingshit.pdf"
+files = {'file' : open(fileToUpload,'rb')}
+headers = {
+           'content-type' : 'application/octet-stream',
+           'file-name' : destFileName, 
+           'dest-dir' : destDir, 
+           'user-agent' : 'quiMe-fileUploader'
+          }
+r = requests.get("http://localhost/REGISTER")
+r = requests.Session()
+res = r.post("http://localhost",files=files, headers = headers, stream=True)
+# res.raw.close()
+print(res.text)
