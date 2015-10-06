a0102a0f0c5560b0fd959bc40250a6781a29e5a1
Shrinidh Rao <shrinidhi666@gmail.com>
2015-10-06 06:18:18 +0530

    fixing

--- lib/constants.py
+++ lib/constants.py
@@ -4,9 +4,10 @@ ouiDevices_theBox_isAlive_offline = 0
 
 ouiSync_taskJobs_status_pending = 0
 ouiSync_taskJobs_status_assigned = 1
-ouiSync_taskJobs_status_running = 2
-ouiSync_taskJobs_status_done = 3
-ouiSync_taskJobs_status_failed = 4
+ouiSync_taskJobs_status_assignedPending = 2
+ouiSync_taskJobs_status_running = 3
+ouiSync_taskJobs_status_done = 4
+ouiSync_taskJobs_status_failed = 5
 
 
 ouiSync_hosts_isAlive_online = 1
@@ -15,6 +16,7 @@ ouiSync_hosts_isAlive_offline = 0
 ouiSync_hosts_enabled_enabled = 1
 ouiSync_hosts_enabled_disabled = 0
 
+ouiSync_clientListenPort = 8895
 
 
 
--- syncClient/client.py
+++ syncClient/client.py
@@ -18,11 +18,9 @@ import dbOuiDevices
 import dbOuiSync
 import constants 
 
-dbconnDevices = dbOuiDevices.db()
-dbconnSync = dbOuiSync.db()
-rawBoxes = dbconnDevices.execute("select * from theBox",dictionary=True)
-rawTasks = dbconnSync.execute("select * from tasks",dictionary=True)
-rawTaskJobs = dbconnSync.execute("select * from taskJobs",dictionary=True)
+
+# rawTasks = dbconnSync.execute("select * from tasks",dictionary=True)
+# rawTaskJobs = dbconnSync.execute("select * from taskJobs",dictionary=True)
 
 rsync = "rsync -v --relative --recursive --append --inplace --checksum --copy-links --xattrs --perms --progress --rsh=/usr/bin/ssh"
 #--delete-after
@@ -30,9 +28,8 @@ rsync = "rsync -v --relative --recursive --append --inplace --checksum --copy-li
 def clientPort():
   while(1):
     try:
-      hostName,ipAddr = getLocalNameIP()
       serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      serverSocket.bind(("", 8895))
+      serverSocket.bind(("", constants.ouiSync_clientListenPort))
       serverSocket.listen(5)
       break
     except:
@@ -47,7 +44,10 @@ def clientPort():
     data = data.rstrip()
     data = data.lstrip()
     if(data == "ISALIVE"):
-      clientSocket.send("ALIVE")
+      try:
+        clientSocket.send("ALIVE")
+      except:
+        print(str(sys.exc_info()))
     clientSocket.close()
 
 
@@ -81,14 +81,19 @@ def getLocalIDIP():
 
 
 def update():
+  dbconnDevices = dbOuiDevices.db()
+  dbconnSync = dbOuiSync.db()
   hostid , ip, totalCpus = getLocalIDIP()
   try:
-    dbconnSync.execute("insert into hosts (hostid,ip,cpuTotal,isAlive) value ('"+ str(hostid).rstrip().lstrip() +"','"+ str(ip).rstrip().lstrip() +"','"+ str(totalCpus).rstrip().lstrip() +"','"+ str(constants.ouiSync_hosts_isAlive_online) +"')")
+    dbconnSync.execute("insert into hosts (hostid,ip,cpuTotal,isAlive) value ('"+ str(hostid).rstrip().lstrip() +"','"+ str(ip).rstrip().lstrip() +"','"+ str(totalCpus).rstrip().lstrip() +"','"+ str(constants.ouiSync_hosts_isAlive_online) +"') \
+                       on duplicate key update ip='"+ str(ip).rstrip().lstrip() +"', cpuTotal='"+ str(totalCpus) +"', isAlive='"+ str(constants.ouiSync_hosts_isAlive_online) +"'")
   except:
     print(str(sys.exc_info()))
 
 
 def doSync(syncDict):
+  dbconnDevices = dbOuiDevices.db()
+  dbconnSync = dbOuiSync.db()
   hostid , ip, totalCpus = getLocalIDIP()
   try:
     dbconnSync.execute("update taskJobs set status = "+ str(constants.ouiSync_taskJobs_status_running) +" where hostid = '"+ str(hostid) +"' and theBoxId = '"+ str(syncDict['theBoxId']) +"' and checksum = '"+ str(syncDict['checksum']) +"'")
@@ -103,7 +108,14 @@ def doSync(syncDict):
       out = os.system(rsynccmd)
     except:
       print(str(sys.exc_info()))
-    dbconnSync.execute("update taskJobs set status = 3 where theBoxId = '"+ str(syncDict['theBoxId']) +"' and checksum = '"+ str(syncDict['checksum']) +"'")
+    try:
+      dbconnSync.execute("update taskJobs set status = "+ str(constants.ouiSync_taskJobs_status_done) +" where theBoxId = '"+ str(syncDict['theBoxId']) +"' and checksum = '"+ str(syncDict['checksum']) +"'")
+    except:
+      print(str(sys.exc_info()))
+    try:
+      dbconnSync.execute("update hosts set cpuFree = cpuFree+1 where id = '"+ str(hostid) +"'")
+    except:
+      print(str(sys.exc_info()))
     print("exit status of the sync : "+ str(out))
     
 
@@ -113,20 +125,35 @@ def doSync(syncDict):
 
 
 def doSyncProcess():
+  dbconnDevices = dbOuiDevices.db()
+  dbconnSync = dbOuiSync.db()
   hostid , ip, totalCpus = getLocalIDIP()
   procpool = Pool(processes=int(totalCpus))
   jobs = []
   while (1):
     try:
-      taskJobsAssigned = dbconnSync.execute("select * from taskJobs where status = 1 and hostid = '"+ str(hostid) +"'",dictionary=True)
+      taskJobsAssigned = dbconnSync.execute("select * from taskJobs where status = "+ str(constants.ouiSync_taskJobs_status_assigned) +" and hostid = '"+ str(hostid) +"'",dictionary=True)
       if(not isinstance(taskJobsAssigned,int)):
         for x in taskJobsAssigned:
           proc = procpool.apply_async(func=doSync,args=(x,))
           jobs.append(proc)
+    except:
+      print(str(sys.exc_info()))
     time.sleep(1)
 
 
         
+
+
+
+def doMain():
+  update()
+  clientServ = multiprocessing.Process(target=clientPort) #,args=(frameInfo,frameScrutiny,cpuAffiToSend,)
+  doSyncServ = multiprocessing.Process(target=doSyncProcess)
+  clientServ.start()
+  doSyncServ.start()
+  clientServ.join()
+  doSyncServ.join()
 
 
 
