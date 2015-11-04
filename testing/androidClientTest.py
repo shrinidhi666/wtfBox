f1ea87f5a486f6c25dc327195a51a56800ff8090
Shrinidhi Rao <shrinidhi666@gmail.com>
2015-11-04 18:27:41 +0530

    fixing

--- testing/androidClientTest.py
+++ testing/androidClientTest.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 import requests
 import snowflake
+import sys
 
+ip = sys.argv[1]
 userdata = {}
 userdata['user'] = "shrinidhi"
 userdata['email'] = "shrinidhi@wtf.com"
 userdata['phone'] = "0987654321"
 userdata['deviceid'] = snowflake.snowflake()
-a = requests.get("http://117.219.148.79/REGISTER",headers=userdata)
+a = requests.get("http://"+ str(ip) +"/REGISTER",headers=userdata)
 print(a.json())
