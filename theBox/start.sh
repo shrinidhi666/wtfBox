1a357bd05359b261b1039c82bc09016325133d0c
K Shrinidhi Rao <shrinidhi666@gmail.com>
2015-11-05 18:42:05 +0530

    fixing

--- theBox/start.sh
+++ theBox/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 /sbin/fsck.vfat -a /dev/sda1
+cd /root/dlink/bin/
 sleep 10
 killall pppd
 sleep 5 
-/usr/sbin/pppd debug call /root/dlink/bin/linux_dial
+/usr/sbin/pppd debug call linux_dial
 sleep 10
 cd ${DIR} 
 /usr/bin/git pull
