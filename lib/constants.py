e5829635eaf48d8bc809c0fa95ee0f5edc559386
Shrinidh Rao <shrinidhi666@gmail.com>
2015-12-10 10:02:06 +0530

    fixing database

--- lib/constants.py
+++ lib/constants.py
@@ -2,12 +2,12 @@
 ouiDevices_theBox_isAlive_online = 1
 ouiDevices_theBox_isAlive_offline = 0
 
-ouiSync_taskJobs_status_pending = 0
-ouiSync_taskJobs_status_assigned = 1
-ouiSync_taskJobs_status_assignedPending = 2
-ouiSync_taskJobs_status_running = 3
-ouiSync_taskJobs_status_done = 4
-ouiSync_taskJobs_status_failed = 5
+ouiSync_tasks_status_pending = 0
+ouiSync_tasks_status_assigned = 1
+ouiSync_tasks_status_assignedPending = 2
+ouiSync_tasks_status_running = 3
+ouiSync_tasks_status_done = 4
+ouiSync_tasks_status_failed = 5
 
 
 ouiSync_hosts_isAlive_online = 1
